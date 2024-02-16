779c7a32eee5f7f0ca371c557ac7d6100dfae7b5
andreluisklr <84353374+andreluisklr@users.noreply.github.com>
2024-02-16 20:56:00 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let nomeHeroi = 'Hunk';
+let  xpHeroi = 10000;
+let nivel = '';
+
+if(xpHeroi < 1000) nivel = 'Ferro';
+if(xpHeroi >= 1001 && xpHeroi <= 2000) nivel = 'Bronze';
+if(xpHeroi >= 2001 && xpHeroi <= 5000) nivel = 'Prata';
+if(xpHeroi >= 5001 && xpHeroi <= 7000) nivel = 'Ouro';
+if(xpHeroi >= 7001 && xpHeroi <= 8000) nivel = 'Platina';
+if(xpHeroi >= 8001 && xpHeroi <= 9000) nivel = 'Ascendente';
+if(xpHeroi >= 9001 && xpHeroi <= 10000) nivel = 'Imortal';
+if(xpHeroi >= 10001) nivel = 'Radiante';
+
+console.log('O Herói de nome ' + nomeHeroi + ' está no nível de ' + nivel);
